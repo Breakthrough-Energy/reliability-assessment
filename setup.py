2648dd5ee5aac7ce204737fa9904ffb414f7ac78
Ben RdO <ben.rouilledorfeuil@breakthroughenergy.org>
2021-06-14 14:24:14 -0700

    chore: add package related files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+install_requires = ["numpy"]
+
+setup(
+    name="reliabilityassessment",
+    version="0.1",
+    description="Grid Reliability Assessment",
+    url="https://github.com/Breakthrough-Energy/reliability-assessment",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=install_requires,
+)
