e86283f78d4a7c95f2e17748a01c60e9fdb6cf5e
yonglizhu <yzhu16@vols.utk.edu>
2021-08-12 15:28:23 -0500

    Update 'plhr'
    remove redundant definition of MXPLHR; rephrase docstring.

--- plhr.py
+++ plhr.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def plhr(HRLOAD):
     """ 
-    Finds the pool (i.e., "all areas") peak hours.
+    Find the pool (i.e., "all areas") peak hours.
     
     :param array HRLOAD: 2D array of hourly load data 
-                            (0-dim: area index, 1-dim: hour index (1~8760hr))
-    :return: (*array*) MXPLHR -- array of the hour of the peak load for the pool (all areas)
+                         (0-dim: area index, 1-dim: hour index (1~8760 hr))
+    :return: (*np.array*) MXPLHR -- array of the peak hour for the pool
     
     Note: the hardcoded number "365" and "24" are OK in this code package. 
     Because for Monte Carlo Simulation in reliability analysis area, researchers do not 
@@ -15,11 +15,9 @@ def plhr(HRLOAD):
     a concern in this area.
      
     In other words, the reliability indices is an overall measure of the studied 
-    system. It is not defined for a specific year. For example, researchers will NOT say 
-    "A reliability index for the year 202O equals XX, for the year 2021 it equals YY, ...".
+    system. It is not defined for any specific year. For example, researchers will NOT say 
+    "a reliability index for the year 202O equals XX, for the year 2021 it equals YY, ...".
     """ 
-
-    MXPLHR = np.zeros((365,1)).astype(int)
                
     # sum across all areas
     summed_load = HRLOAD.sum(axis=0)
