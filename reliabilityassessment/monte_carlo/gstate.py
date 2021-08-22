ecdd2083e8d51ec34117b4c001a12e9f3ab68fb0
yonglizhu <yzhu16@vols.utk.edu>
2021-08-22 17:24:27 -0500

    refactor: update docstring and move subfolders
    move the two subfolders into a parent folder 'reliabilityassessment' ;update some docstring; remove obsolete return value.

--- reliabilityassessment/monte_carlo/gstate.py
+++ reliabilityassessment/monte_carlo/gstate.py
@@ -4,25 +4,22 @@ def gstate(PROBG, RATING, DERATE, PLNDST):
     """
     It samples the state of available capacity for each generator up to NUNITS
     
-    :param array PROBG: 2D array of accumulated probability for each capacity tier of each unit
-    :param array DERATE: array of the derated capacity of each unit
-    :param array RATING:  array of the fully rated capacity of each unit
-    :param array PLNDST: array of the on/off status (due to planned maintenance) of each unit
+    :param numpy.ndarray PROBG: 2D array of accumulated probability for each capacity tier of each unit
+    :param numpy.ndarray DERATE: array of the derated capacity of each unit
+    :param numpy.ndarray RATING:  array of the fully rated capacity of each unit
+    :param numpy.ndarray PLNDST: array of the on/off status (due to planned maintenance) of each unit
     
-    :return: PCTAVL (array) -- array of unadjusted power capacity (in p.u. value) of each unit
-             AVAIL (array) -- array of the finalized power capacity (in nominal value) 
-                              of each unit             
+    :return: :return: (*tuple*) -- a pair of numpy.ndarray objects for unadjusted power capacity in p.u. 
+                                   of each unit and finalized power capacity in MW of each unit.
+    
     Note: neither rng.choice nor vectorization trick is appropriate here due to 
           the following reasons:
           1) The capacity probability of each gen unit CAN BE different with each other’s. 
              That is why the PROBG array is originally designed as 2-D but not 1-D.
           2) The rng.choice function cannot (or can but awkwardly) adapt to the situation here due to 1); 
-          3) Most of all: generating a random vector and applying vectorized operation may 
-             break a critical implicit requirement for the Monte Carlo simulation -- 
-             "generated random numbers should be mutually independent."
     """    
     NUNITS = len(PROBG)
-    print("Enter subroutine gstate, total number of Gen units is %d" % (NUNITS))
+    print("Enter function gstate, total number of Gen units is %d" % (NUNITS))
     
     PCTAVL = np.zeros((NUNITS,1))
     AVAIL = np.zeros((NUNITS,1))
@@ -41,4 +38,4 @@ def gstate(PROBG, RATING, DERATE, PLNDST):
             
         AVAIL[i] = PCTAVL[i] * RATING[i] * PLNDST[i]
             
-    return PCTAVL, AVAIL, RANDG
+    return PCTAVL, AVAIL
