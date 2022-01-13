96755c2e667239d24e38a8117789317a4624be16
Daniel Muldrew <dmuldrew@gmail.com>
2022-01-13 11:00:54 -0800

    chore: black formatting

--- reliabilityassessment/monte_carlo/thetac.py
+++ reliabilityassessment/monte_carlo/thetac.py
@@ -1,20 +1,19 @@
-
-def thetac(THET,THETC,LT,NN):
+def thetac(THET, THETC, LT, NN):
     """
     Generates vector of bus angles for all buses including ref bus
-    
+
     :param numpy.ndarray THET: angle at node i of the ADM matrix
     :param numpy.ndarray THETC: angle for actual node corresponding to node i of the ADM matrix
     :param numpy.ndarray LT: actual node corresponding to node i of the ADM matrix
     :param int NN: number of nodes
-    
+
     :return: (*None*)
     """
     for i in range(NN):
-        THETC[i] = 0.
-    
-    for i in range(NN-1):
+        THETC[i] = 0.0
+
+    for i in range(NN - 1):
         j = LT[i]
         THETC[j] = THET[i]
-    
-    return # THETC; no need for explictly return since it is array (ref type)
+
+    return  # THETC; no need for explictly return since it is array (ref type)
