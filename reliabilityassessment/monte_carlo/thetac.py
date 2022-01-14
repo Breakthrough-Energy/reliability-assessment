46c0ddd176de0de67c8e1aa5bb169513757dec2d
Daniel Muldrew <dmuldrew@gmail.com>
2022-01-14 09:47:58 -0800

    refactor: remove unnecessary return

--- reliabilityassessment/monte_carlo/thetac.py
+++ reliabilityassessment/monte_carlo/thetac.py
@@ -6,8 +6,6 @@ def thetac(THET, THETC, LT, NN):
     :param numpy.ndarray THETC: angle for actual node corresponding to node i of the ADM matrix
     :param numpy.ndarray LT: actual node corresponding to node i of the ADM matrix
     :param int NN: number of nodes
-
-    :return: (*None*)
     """
     for i in range(NN):
         THETC[i] = 0.0
@@ -15,5 +13,3 @@ def thetac(THET, THETC, LT, NN):
     for i in range(NN - 1):
         j = LT[i]
         THETC[j] = THET[i]
-
-    return  # THETC; no need for explictly return since it is array (ref type)
