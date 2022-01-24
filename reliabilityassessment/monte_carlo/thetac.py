26cb97deff94b30b6f1825ce88f92afc58b9a41b
Daniel Muldrew <dmuldrew@gmail.com>
2022-01-24 12:43:19 -0800

    refactor: THETC as return

--- reliabilityassessment/monte_carlo/thetac.py
+++ reliabilityassessment/monte_carlo/thetac.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 
-def thetac(THET, THETC, LT):
+def thetac(THET, LT):
     """
-    Generates vector of bus angles for all buses including ref bus
+    Generates vector of bus angles for all buses including ref bus. This function is used during step 7 in
+    the General Program Logic section of the project README.
 
     :param numpy.ndarray THET: angle at every node of the ADM matrix
-    :param numpy.ndarray THETC: angle of actual node corresponding to each node of the ADM matrix
     :param numpy.ndarray LT: actual node corresponding to node i of the ADM matrix
-    :param int NN: number of nodes
+
+    :return: (*numpy.ndarray*) -- THETC: angle of actual node corresponding to each node of the ADM matrix
     """
-    NN = len(THETC)
+    NN = len(THET)
     THETC = np.zeros(NN)
 
     # last array value is the "reference" bus that ALWAYS has a zero phase angle
     THETC[LT[:-2]] = THET[:-2]
+    return THETC
