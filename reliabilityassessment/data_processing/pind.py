669f21927e55c595b742ff8246c6f4bcee887b96
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-03-02 21:57:45 -0600

    feat and test: create function 'pind'
    and its and unit test

---
+++ reliabilityassessment/data_processing/pind.py
@@ -0,0 +1,78 @@
+from copy import deepcopy
+
+import numpy as np
+
+
+def pind(inputB_dict):
+    """
+    Mapping name strings (of the areas, gen units, transmission link (lines)) to integer (indices)
+
+    :param dict inputB_dict: resulted dictionary from 'readInputB' function
+
+    :param np.array ID:
+        ID(I,K),K=0 to 2 : UNIT NUMBER, PLANT NUMBER, AREA OF LOCATION
+                K=3 STARTING WEEK OF FIRST PLANNED OUTGE
+                K=4 DURATION OF FIRST PLANNED OUTAGE IN WEEKS
+                K=5 STARTING WEEK OF SECOND PLANNED OUTAGE
+                K=6 DURATION OF SECOND PLANNED OUTAGE
+                K=7 1 IF MAINTENANCE IS PRESCHEDULED AND 0 IF
+                AUTOMATIC BY THE PROGRAM.
+
+    :return: (*dict*) inputB_dict_nameToInt -- A dictionary storing the same content as the 'inputB_dict',
+                         except that all string-type 'names' are mapped to interger indices
+                         based on their showing orders in the original INPUTB file.
+    """
+    inputB_dict_nameToInt = deepcopy(inputB_dict)
+
+    # For ZZLD:
+    NAR = inputB_dict_nameToInt["ZZLD"]["NAR"]
+    unique_list = list(dict.fromkeys(NAR))
+    map_AreaNameToInt = dict(zip(unique_list, range(len(unique_list))))
+    inputB_dict_nameToInt["ZZLD"]["NAR"] = np.array(
+        [map_AreaNameToInt[key] for key in NAR]
+    )
+
+    # For ZZUD:
+    NAT = inputB_dict_nameToInt["ZZUD"]["NAT"]
+    unique_list = list(dict.fromkeys(NAT))
+    map_GenUnitNameToInt = dict(zip(unique_list, range(len(unique_list))))
+    inputB_dict_nameToInt["ZZUD"]["NAT"] = np.array(
+        [map_GenUnitNameToInt[key] for key in NAT]
+    )
+
+    NAR = inputB_dict_nameToInt["ZZUD"]["NAR"]
+    inputB_dict_nameToInt["ZZUD"]["NAR"] = np.array(
+        [map_AreaNameToInt[key] for key in NAR]
+    )
+
+    # For ZZTD:
+    inputB_dict_nameToInt["ZZTD"]["LineID"] -= 1
+
+    NAR = inputB_dict_nameToInt["ZZTD"]["NAR"]
+    inputB_dict_nameToInt["ZZTD"]["NAR"] = np.array(
+        [map_AreaNameToInt[key] for key in NAR]
+    )
+
+    NAE = inputB_dict_nameToInt["ZZTD"]["NAE"]
+    inputB_dict_nameToInt["ZZTD"]["NAE"] = np.array(
+        [map_AreaNameToInt[key] for key in NAE]
+    )
+
+    # For ZZFC:
+    NAR = inputB_dict_nameToInt["ZZFC"]["NAR"]
+    inputB_dict_nameToInt["ZZFC"]["NAR"] = np.array(
+        [map_AreaNameToInt[key] for key in NAR]
+    )
+
+    NAE = inputB_dict_nameToInt["ZZFC"]["NAE"]
+    inputB_dict_nameToInt["ZZFC"]["NAE"] = np.array(
+        [map_AreaNameToInt[key] for key in NAE]
+    )
+
+    # For ZZOD:
+    NAT = inputB_dict_nameToInt["ZZOD"]["NAT"]
+    inputB_dict_nameToInt["ZZOD"]["NAT"] = np.array(
+        [map_GenUnitNameToInt[key] for key in NAT]
+    )
+
+    return inputB_dict_nameToInt
