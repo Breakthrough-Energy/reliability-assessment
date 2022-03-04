b4d0608d55e45f8c1e5f2f5d32303e114e974e22
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-03-04 14:34:50 -0600

    fix and doc: enrich doc string and fix bugs

--- reliabilityassessment/monte_carlo/admitb.py
+++ reliabilityassessment/monte_carlo/admitb.py
@@ -1,39 +1,37 @@
-import numpy as np
-
-def admitb(LP, BLP):
+def admitb(LP, BLP, BB):
     """
-    :param np.ndarray LP: line connection data
-        LP[L,K]: L, ENTRY NUMBER; K=1 line number,
-                  K=2 starting area, K=3 ending area.
-    :param np.ndarray BLP: (adjusted) line data:
-        BLP[I,J]: Six sets of three entries - admitance,forward and 
-              backward capacities for six states.
-              (refer to file 'INPUTB' for more details)
+    Construct the admittance matrix (B) from the line data array (BLP)
 
-    :return: (*np.ndarray*) BB -- 2D array of the 'B matrix' (i.e., admittance matrix) 
-                                  size: # Area-by-# Area
+    :param numpy.ndarray LP: array storing line related information
+                            shape: (NLINES, 3)
+                            LP(I,J)  I  ENTRY  NUMBER
+                            LP(I,1)  LINE NUMBER SET=I
+                            LP(I,2)  STARTING NODE
+                            LP(I,3)  ENDING NODE
+    :param numpy.ndarray BLP: contains the data on admittance, cap and backward cap
+                              of the line at position I in LP(I,J) (at the state-1)
+                              shape: (NLINES, 3)
+                              BLP(I,J) I  ENTRY  NUMBER
+                              BLP(I,1)  admittance
+                              BLP(I,2)  capacity (MW)
+                              BLP(I,3)  backward capacity (MW)
+
+    :param numpy.ndarray BB: 2D array of the 'B matrix' (i.e., admittance matrix)
+                             used in DC power flow
+                             shape (NOAREA, NOAREA)
+
+    :return: (*none*)
     """
-    #BLP = zeros((100,3))
-    #LP = zeros((100,3))
-    BB = np.zeros((20,20)) # An exilicit 'NAREA' parameter maybe used here
-    
-    for i in range(()):
-        for j in range(()):
-            BB[i,j] = 0.0
-    
+    BB[:] = 0.0
+
     i = 0
-    while(LP[i,0] != 0): # End of data detected by line #0
-        j = LP(i,1) # from-bus id
-        k = LP(i,2) # to-bus id
-        BB[j,k] = BB[j,k] + BLP[i,0] * (-1.0)
-        BB[k,j] = BB[j,k]
-        BB[j,j] = BB[j,j] + BLP[i,0]
-        BB[k,k] = BB[k,k] + BLP[i,0]
+    while LP[i, 0] != -1:  # End of data detected by integer -1
+        j = LP(i, 1)  # from-bus (area) id
+        k = LP(i, 2)  # to-bus (area) id
+        BB[j, k] += BLP[i, 0] * (-1.0)
+        BB[k, j] = BB[j, k]
+        BB[j, j] += BLP[i, 0]
+        BB[k, k] += BLP[i, 0]
         i = i + 1
 
     return BB
-    
-    
-    
-    
-    
