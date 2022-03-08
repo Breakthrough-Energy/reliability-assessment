d088dc7f6b6eb68c5f573558fc51edd58d392fc8
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-03-08 12:56:23 -0600

    test: add extra unit test

--- reliabilityassessment/monte_carlo/tests/test_flo.py
+++ reliabilityassessment/monte_carlo/tests/test_flo.py
@@ -18,3 +18,18 @@ def test_flo():
     SFLOW, FLOW = flo(LP, BLP, THET)
     np.testing.assert_array_almost_equal(FLOW, FLOW_)
     np.testing.assert_array_almost_equal(SFLOW, SFLOW_)
+
+    # NOAREA = 3
+    # NLINES = 2
+    # SFLOW = np.zeros((NOAREA,))
+    # FLOW = np.zeros((NLINES,))
+    LP = np.array([[0, 0, 1], [1, 2, 0]], dtype=int)
+    BLP = np.array([[-120, 300, 300], [-80, 100, 100]])
+    THET = np.array([0.1, 0.2, 0.3])  # shape: (NOAREA,)
+
+    FLOW_ = np.array([12.0, -16.0])
+    SFLOW_ = np.array([-28.0, 12.0, 16.0])
+
+    SFLOW, FLOW = flo(LP, BLP, THET)
+    np.testing.assert_array_almost_equal(FLOW, FLOW_)
+    np.testing.assert_array_almost_equal(SFLOW, SFLOW_)
