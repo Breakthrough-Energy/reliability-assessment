7608e98723c5f86d3939d89f00846c835e090ee2
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-03-07 19:50:31 -0600

    PR for creating function 'flo' (#29)
    * feat: create function 'flo'
    
    * fix: update the logic and fix bugs
    
    LP  initial value should be -1 since it can be used as index;
    also remove redundant 'return'
    
    * refactor and fix: fix bugs due to while loop
    
    bug of indexing error.
    
    * refactor and doc: add SFLOW as return value
    
    also improve readability of docstring and the for-loop
    
    * test and fix: add unit test
    
    and fix missing return value bug

---
+++ reliabilityassessment/monte_carlo/flo.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def flo(LP, BLP, FLOW, THET):
+    """
+    Computes line power flows and bus (power) injection
+
+    :param numpy.ndarray LP: array storing line related information
+                            shape: (NLINES, 3)
+                            LP(I,J)  I  ENTRY  NUMBER
+                            LP(I,0)  LINE NUMBER SET=I
+                            LP(I,1)  STARTING NODE
+                            LP(I,2)  ENDING NODE
+
+    :param numpy.ndarray BLP: contains the data on admittance, cap and backward cap
+                              of the line at position I in LP(I,J) (at the state-1)
+                              shape: (NLINES, 3)
+                              BLP(I,J) I  ENTRY  NUMBER
+                              BLP(I,0)  admittance
+                              BLP(I,1)  capacity (MW)
+                              BLP(I,2)  backward capacity (MW)
+
+    :param numpy.ndarray FLOW: vector of line power flows (MW),  shape: (NLINES, )
+
+    :param numpy.ndarray THET: bus (area) angle, shape: (NOAREA, )
+
+    :return: (*numpy.ndarray*) SFLOW -- vector of bus (area) injection power (MW)
+                                        shape (NOAREA, )
+    """
+    NLINES = LP.shape[0]
+    SFLOW = np.zeros((NLINES,))
+    for i in range(NLINES):
+        j = LP[i, 1]  # from bus (area)
+        k = LP[i, 2]  # to bus (area)
+        FLOW[i] = (THET[j] - THET[k]) * BLP[i, 0]
+        SFLOW[k] += FLOW[i]
+        SFLOW[j] -= FLOW[i]
+
+    return SFLOW
