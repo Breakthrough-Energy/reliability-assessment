9aa43c8df93a75603f8fc7342fc9e2b7f1f52881
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-03-09 17:12:42 -0600

    feat: create function 'initfl'

---
+++ reliabilityassessment/monte_carlo/initfl.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def initfl():
+    """
+    Initialize array EVNTS and several related pointers
+
+    return (*tuple*) --
+                     MFA: pointer of first available (i.e. unoccupied) slot in the event list
+                     IPOINT: pointer of the first already occupied slot (i.e. an stored entry)
+                             in the event list
+                             initial value = -1
+                     EVNTS: 1D array of event list
+    """
+
+    MFA = 0
+    IPOINT = -1
+    MXCELL = 100  # this value is predefined and adjustable; it won't change during simulation.
+    EVNTS = np.zeros(MXCELL)
+
+    # array slots starting from MXCELL – 4 (1-based index) are 0 after initialization.
+    EVNTS[: MXCELL - 4 : 4] = np.arange(0, MXCELL - 4, 4) + 4
+
+    return MFA, IPOINT, EVNTS
