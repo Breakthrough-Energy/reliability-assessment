a3f22a8db8d07f7fe697c5f821c4ac8f4b5f15f7
BainanXia <bainan.xia@breakthroughenergy.org>
2022-04-14 18:37:47 -0500

    feat: add simplified version of 'filem' and expand test coverage

--- reliabilityassessment/monte_carlo/filem.py
+++ reliabilityassessment/monte_carlo/filem.py
@@ -61,3 +61,45 @@ def _filem(MFA, ATRIB, NUMINQ, IPOINT, EVNTS):
         IPOINT = NEW
         EVNTS[NEW] = -1
         return NUMINQ, MFA, IPOINT
+
+
+def filem(MFA, ATRIB, NUMINQ, IPOINT, EVNTS):
+    """
+    Append a new event into the event list, maintain chronological order and update
+    corresponding pointers
+
+    :param int MFA: pointer of the first available (i.e. empty) entry in the event list
+    :param numpy.ndarray ATRIB: events attribute vector of length 2
+                                ATRIB[0] -- global simulation clock (in unit: hour)
+                                ATRIB[1] -- int, simulation type
+                                 0: hourly
+                                 1: weekly
+                                 2: quarterly
+                                 3: yearly
+    :param int NUMINQ: up-to-date total number of event entries (inquires)
+    :param int IPOINT: pointer of the first (already/previously stored) entry in the
+        event list, defaults to -1 if the list is empty
+    :param numpy.ndarray EVNTS: 1D array of events
+    :return: (*tuple*) -- updated NUMINQ, MFA and IPOINT
+
+    .. note:: EVNTS is modified in place
+    """
+
+    NUMINQ += 1
+    EVNTS[MFA + 1 : MFA + 3] = ATRIB
+    time_rank = EVNTS[: NUMINQ * 4][1::4].argsort().argsort()
+    # the new event is the first entry in chronological order
+    if time_rank[-1] == 0:
+        EVNTS[MFA] = IPOINT
+        IPOINT = MFA
+    # the new event is the last entry in chronological order
+    elif time_rank[-1] == NUMINQ - 1:
+        EVNTS[MFA] = -1
+        EVNTS[np.where(time_rank == NUMINQ - 2)[0][0] * 4] = MFA
+    # the new event is one of the middle entries in chronological order
+    else:
+        EVNTS[MFA] = np.where(time_rank == time_rank[-1] + 1)[0][0] * 4
+        EVNTS[np.where(time_rank == time_rank[-1] - 1)[0][0] * 4] = MFA
+    MFA += 4
+
+    return NUMINQ, MFA, IPOINT
