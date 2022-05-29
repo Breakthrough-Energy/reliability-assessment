6f214ab8c2ac9e0c2545c8fa4e38350a3162fc1f
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-05-29 11:38:12 -0500

    feat: create function 'findld' (#36)

---
+++ reliabilityassessment/monte_carlo/findld.py
@@ -0,0 +1,20 @@
+def findld(NFACT, JHOUR, HRLOAD, FCTERR):
+    """
+    Determine the load needs of each area under the specific load forecast error factor
+
+    :param: int NFACT: the index of the forecast error factor to be used
+    :param: int JHOUR: the hour index in the annual load profile
+    :param: numpy.ndarray HRLOAD: 2D array with shape (NOAREA, 8760), the hourly
+        annual load profile
+    :param: numpy.ndarray FCTERR: 2D array with shape shape (NOAREA, 5), the load
+        forecast error factors
+    :return: (*numpy.ndarray*) -- 1D array with shape (NOAREA, ), the capacity required
+        for each area at the hour indexed by JOUHR, i.e. CAPREQ(J) passed to
+        transmission module
+
+     .. note:: in the original Fortran code, CAPREQ is annotated as "LOAD IN AREA J"
+    """
+
+    print("Entering function findld")
+
+    return HRLOAD[:, JHOUR] * FCTERR[:, NFACT]
