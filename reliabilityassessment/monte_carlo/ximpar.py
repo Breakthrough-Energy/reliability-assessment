451a01674a758f108e866aad3d26a7d3aff12b14
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-08-30 16:37:03 -0500

    test and doc: add unit test

--- reliabilityassessment/monte_carlo/ximpar.py
+++ reliabilityassessment/monte_carlo/ximpar.py
@@ -3,14 +3,13 @@ import numpy as np
 
 def ximpar(ZB, ZIJ, NI, NN):
     """
-    Modifies ZBUS matrix if impedance ZIJ added between bus NI and bus REF
+    Modifies ZBUS matrix if impedance ZIJ added between bus NI and REF
 
     :param np.array ZB: the input ZBUS matrix  (dim: areas-by-areas)
+    :param int ZIJ: the impedance to be added to the system
     :param int NI: from bus id of the newly added branch
-    :param int NJ: to bus id of the newly added branch
     :param int NN: total bus number
-
-    return Z -- the modified ZBUS matrix (dim: areas-by-areas)
+    :return: (*numpy.ndarray*) -- the modified ZBUS matrix (dim: areas-by-areas)
     """
 
     ZCOL = np.zeros(NN)
@@ -22,6 +21,6 @@ def ximpar(ZB, ZIJ, NI, NN):
     Z = np.zeros((NN, NN))
     for i in range(NN):
         for j in range(NN):
-            Z[i, j] = ZB[i, j] - [ZCOL[i] * ZCOL[j]] / ZN1
+            Z[i, j] = ZB[i, j] - (ZCOL[i] * ZCOL[j]) / ZN1
 
     return Z
