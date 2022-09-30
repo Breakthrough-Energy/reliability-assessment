d760224766ac08b0643da39fa43c39392e23466b
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-09-29 20:37:31 -0500

    test: add unit tests
    update the calling signature of a child function

--- reliabilityassessment/monte_carlo/dgeco.py
+++ reliabilityassessment/monte_carlo/dgeco.py
@@ -3,48 +3,53 @@ import numpy as np
 from reliabilityassessment.monte_carlo.dgefa import dgefa
 
 
-def dgeco(A, LDA, N):
+def dgeco(A, N):
     """
     Factors a double precision matrix by gaussian elimination
     and estimates the condition of the matrix.
 
     :param numpy.ndarray A: the matrix to be factored
-    :param int LDA: the leading dimension of A
     :param int N: the order of the matrix  A
 
     :return: (*numpy.ndarray*) IPVT -- an integer vector of pivot indices.
                                        shape (N,)
 
-    .. note:: 1) arrays are modified in place.
+    .. note:: 1) array A is modified in place.
               2) if RCOND is not needed, dgefa is slightly faster.
               3) on return, A  will be an upper triangular matrix and the multipliers
-              which were used to obtain it. the factorization can be written  A = L*U,
+              used to obtain it. The factorization can be written  A = L*U,
               where L is a product of permutation and unit lower triangular matrices
               and  U  is upper triangular.
               4) RCOND and Z are not used outside
               :param float RCOND: an estimate of the reciprocal condition of A.
                         for the system  A*x = b , relative perturbations
-                        in  A  and  b  of size  epsilon  may cause
+                        in  A  and  b  of size, epsilon  may cause
                         relative perturbations in  x  of size  epsilon/RCOND.
                         if  RCOND  is so small that the logical expression
                                    1.0 + RCOND == 1.0
                         is true, then A  may be singular to working
-                        precision.  in particular,  RCOND  is zero  if
-                        exact singularity is detected or the estimate
+                        precision. In particular,  RCOND  is zero if
+                        the exact singularity is detected or the estimate
                         underflows.
               :param numpy.ndarray Z: a work vector whose contents are usually unimportant.
                             If A is close to a singular matrix, then  Z is
-                            An approximate null vector in the sense that
+                            an approximate null vector in the sense that
                             Norm(A*Z) = RCOND*norm(A)*norm(Z) .
     """
+    # Some intermediate variables or original returning variables are never used outside, e.g.
+    # YNORM, ANORM, RCOND and Z
+    # Currently still keep them in case of any special debugging need in the future.
 
-    # Compute 1-norm of A, i.e. is the maximum abs. column sum
+    # Compute 1-norm of A, i.e., is the maximum abs. column sum
     ANORM = 0.0
     for j in range(N):
         ANORM = max(ANORM, sum(abs(A[:, j])))
 
     # Factorization
-    INFO, IPVT = dgefa(A, LDA, N)
+
+    # The 1st return parameter 'INFO' is not used anywhere.
+    # Currently still keep it as a placeholder in case of any special debugging need in the future.
+    _, IPVT = dgefa(A, N)
 
     EK = 1.0
     Z = np.zeros(N)
@@ -84,6 +89,10 @@ def dgeco(A, LDA, N):
     S = 1.0 / sum(Z)
     Z *= S
 
+    # In the following code, L,U stands for the lower- and upper-triangular decomposition of A;
+    # Y, V are the names for certain intermediate matrices during computation
+    # W stands for (possibly) the "old" /"initial" value of the Z vector
+
     # Solve TRANS(L)*Y = W
     for KB in range(N):
         K = N - 1 - KB
@@ -92,7 +101,7 @@ def dgeco(A, LDA, N):
                 A[K + 1 : N, K], Z[K + 1 : N]
             )  # DDOT(N-1-K,A(K+1,K),1,Z(K+1),1)
         if abs(Z[K]) > 1.0:
-            S = 1.0 / abs(Z(K))
+            S = 1.0 / abs(Z[K])
             Z *= S
         L = IPVT[K]
         Z[L], Z[K] = Z[K], Z[L]
@@ -103,13 +112,13 @@ def dgeco(A, LDA, N):
     YNORM = 1.0
     # Solve L*V = Y
     for K in range(N):
-        L = IPVT(K)
-        T = Z(L)
+        L = IPVT[K]
+        T = Z[L]
         Z[L], Z[K] = Z[K], Z[L]
         if K < N - 1:
             Z[K + 1 : N] += T * A[K + 1 : N, K]  # DAXPY(N-1-K,T,A(K+1,K),1,Z(K+1),1)
         if abs(Z[K]) > 1.0:
-            S = 1.0 / abs(Z(K))
+            S = 1.0 / abs(Z[K])
             Z *= S
             YNORM = S * YNORM
 
@@ -120,24 +129,25 @@ def dgeco(A, LDA, N):
     # Solve  U*Z = V
     for KB in range(N):
         K = N - 1 - KB
-        if abs(Z(K)) > abs(A(K, K)):
-            S = abs(A(K, K)) / abs(Z(K))
+        if abs(Z[K]) > abs(A[K, K]):
+            S = abs(A[K, K]) / abs(Z[K])
             Z *= S
             YNORM = S * YNORM
-        if A(K, K) != 0.0:
+        if A[K, K] != 0.0:
             Z[K] = Z[K] / A[K, K]
-        if A(K, K) == 0.0:
+        if A[K, K] == 0.0:
             Z[K] = 1.0
-        T = -Z(K)
+        T = -Z[K]
         Z[0:K] += T * A[0:K, K]  # DAXPY(K-1,T,A(1,K),1,Z(1),1)
 
     # Make ZNORM = 1.0
     S = 1.0 / sum(Z)
     Z *= S
     YNORM = S * YNORM
+    # The following has to be commented otherwise 'tox -e flake8' shows error for unused 'RCOND'
     # if ANORM != 0.0:
     #     RCOND = YNORM / ANORM
     # else:
     #     RCOND = 0.0
 
-    return IPVT  # RCOND and Z are not used outside
+    return IPVT
