0d426fc25852a200db9da3ad5f4188e6064718cf
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-10-31 15:53:08 -0500

    fix and test: fix bugs in 'tm1' and 'admitr' and add more tests for 'flo' and 'tm1' (#86)
    also update some doc strings for more accurate descriptions.

--- reliabilityassessment/monte_carlo/admitr.py
+++ reliabilityassessment/monte_carlo/admitr.py
@@ -4,29 +4,34 @@ import numpy as np
 # vanilla version
 def _admitr(BB, BN, NR):
     """
-    Remove Ref bus (node) from the admittance matrix
+    Remove REF bus (area) from the admittance matrix
 
-    :param numpy.ndarray BB: 2D array with shape (NOAREA, NOAREA), B Matrix (i.e.
-                             admittance matrix) in DC power flow
-    :param numpy.ndarray BN: 2D array with shape (NOAREA, 4)
-                             BN[I, 0]: area (bus/node) number
-                             BN[I, 1]: area load, overridden by net injection after
-                                       calling :py:func: `tm2`
-                             BN[I, 2]: area net injection, overridden by load
-                                       curtailment after calling :py:func: `tm2`
-                             BN[I, 3]: area constraint on total power flow
+    :param numpy.ndarray BB: 2D array with shape (NOAREA, NOAREA) the admittance
+        matrix 'B' used in DC power flow, in particular, it contains the entire
+        admittance matrix upon input and the REF bus is removed at output
+    :param numpy.ndarray BN: 2D array with shape (NOAREA, 5)
+                             BN[I, 0]: area (i.e., bus) number
+                             BN[I, 1]: load (MW) at area I; will be overridden by net
+                                       injection after calling :py:func: `tm2`
+                             BN[I, 2]: net injection (generation)(MW) at area I; will be
+                                       overridden by load curtailment after calling
+                                       :py:func: `tm2`
+                             BN[I, 3]: max power flow (MW) allowed at area I
+                             BN[I, 4]: a helper value to store modified
+                                       "constraint on sum of flows" at the area I
     :param int NR: index number of the reference node.
-    :return: (*tuple*) -- BB: 2D array with shape (NOAREA, NOAREA)
-                              B Matrix without reference bus in DC power flow
+    :return: (*tuple*) -- a pair of numpy arrays, BB and LT
+                          BB: 2D array with shape (NOAREA, NOAREA)
+                              the 'B matrix without reference bus' in DC power flow
                           LT: 1D array with shape (NOAREA, )
-                              original node index of reduced B matrix, ``BB``
+                              array recording the original bus no.
     """
 
-    NN = BB.shape[0]  # total numebr of buses (i.e., areas)
+    NN = BB.shape[0]  # total number of buses (i.e., areas)
     NX = NN - 1
 
     BT = np.zeros((NX, NX))
-    LT = np.zeros((NN,))
+    LT = np.zeros((NN,), dtype=int)
 
     ii = 0
     for i in range(NN):
@@ -52,25 +57,29 @@ def _admitr(BB, BN, NR):
 # vectorized version
 def admitr(BB, BN, NR):
     """
-    Remove Ref bus (node) from the admittance matrix
+    Remove REF bus (area) from the admittance matrix
 
-    :param numpy.ndarray BB: 2D array of the 'B matrix' (i.e., admittance matrix)
-                                  size: # Area-by-# Area
-                          at input stage, BB  contains the entire YBUS matrix
-                          at output stage, BB  contains YBUS matrix without the REF bus
-    :param numpy.ndarray BN(I,J): for bus (i.e., area) I,
-             J=0   bus number
-             J=1   load (MW) at bus I
-             J=2   generation (MW) at bus  I
-             J=3  max power flow (MW) allowed at bus I
-    :param int NR: bus no. of the Ref bus
-
-    :return: (*tuple*) -- a pair of numpy.ndarray objects, i.e.,
-             BB: updated B matrix (admittance matrix for DC power flow)
-             LT: array recording the original bus no.
+    :param numpy.ndarray BB: 2D array with shape (NOAREA, NOAREA) the admittance
+        matrix 'B' used in DC power flow, in particular, it contains the entire
+        admittance matrix upon input and the REF bus is removed at output
+    :param numpy.ndarray BN: 2D array with shape (NOAREA, 5)
+                             BN[I, 0]: area (i.e., bus) number
+                             BN[I, 1]: load (MW) at area I; will be overridden by net
+                                       injection after calling :py:func: `tm2`
+                             BN[I, 2]: net injection (generation)(MW) at area I; will be
+                                       overridden by load curtailment after calling
+                                       :py:func: `tm2`
+                             BN[I, 3]: max power flow (MW) allowed at area I
+                             BN[I, 4]: a helper value to store modified
+                                       "constraint on sum of flows" at the area I
+    :param int NR: index number of the reference node.
+    :return: (*tuple*) -- a pair of numpy arrays, BB and LT
+                          BB: 2D array with shape (NOAREA, NOAREA)
+                              the 'B matrix without reference bus' in DC power flow
+                          LT: 1D array with shape (NOAREA, )
+                              array recording the original bus no.
     """
-
-    LT = np.append(np.delete(BN[:, 0], NR), BN[NR, 0])
+    LT = np.append(np.delete(BN[:, 0], NR), BN[NR, 0]).astype(int)
     BB = np.delete(np.delete(BB, obj=NR, axis=0), obj=NR, axis=1)
-
+    BB = np.pad(BB, ((0, 1), (0, 1)))
     return BB, LT
--- reliabilityassessment/monte_carlo/tests/test_flo.py
+++ reliabilityassessment/monte_carlo/tests/test_flo.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import numpy as np
+from numpy import loadtxt
 
 from reliabilityassessment.monte_carlo.flo import flo
 
@@ -33,3 +36,115 @@ def test_flo():
     SFLOW, FLOW = flo(LP, BLP, THET)
     np.testing.assert_array_almost_equal(FLOW, FLOW_)
     np.testing.assert_array_almost_equal(SFLOW, SFLOW_)
+
+    # ---------------- Extra unit test cases added in Oct. 2022-------------
+    # NOAREA = 5
+    # NLINES = 5
+    # SFLOW = np.zeros((NOAREA,))
+    # FLOW = np.zeros((NLINES,))
+    TEST_DIR = Path(__file__).parent.absolute()
+
+    # --------------- case 1 --------------
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case1/LP")
+    LP = loadtxt(FileNameAndPath).astype(int)
+    LP -= 1  # 0-based index in python!
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case1/BLP")
+    BLP = loadtxt(FileNameAndPath)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case1/THET")
+    THET = loadtxt(FileNameAndPath)
+
+    SFLOW, FLOW = flo(LP, BLP, THET)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case1/FLOW")
+    FLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(FLOW, FLOW_true)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case1/SFLOW")
+    SFLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(SFLOW, SFLOW_true)
+
+    # --------------- case 2 --------------
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case2/LP")
+    LP = loadtxt(FileNameAndPath).astype(int)
+    LP -= 1  # 0-based index in python!
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case2/BLP")
+    BLP = loadtxt(FileNameAndPath)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case2/THET")
+    THET = loadtxt(FileNameAndPath)
+
+    SFLOW, FLOW = flo(LP, BLP, THET)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case2/FLOW")
+    FLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(FLOW, FLOW_true, decimal=4)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case2/SFLOW")
+    SFLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(SFLOW, SFLOW_true, decimal=4)
+
+    # --------------- case 3 --------------
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case3/LP")
+    LP = loadtxt(FileNameAndPath).astype(int)
+    LP -= 1  # 0-based index in python!
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case3/BLP")
+    BLP = loadtxt(FileNameAndPath)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case3/THET")
+    THET = loadtxt(FileNameAndPath)
+
+    SFLOW, FLOW = flo(LP, BLP, THET)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case3/FLOW")
+    FLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(FLOW, FLOW_true, decimal=4)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case3/SFLOW")
+    SFLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(SFLOW, SFLOW_true, decimal=4)
+
+    # --------------- case 4 --------------
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case4/LP")
+    LP = loadtxt(FileNameAndPath).astype(int)
+    LP -= 1  # 0-based index in python!
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case4/BLP")
+    BLP = loadtxt(FileNameAndPath)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case4/THET")
+    THET = loadtxt(FileNameAndPath)
+
+    SFLOW, FLOW = flo(LP, BLP, THET)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case4/FLOW")
+    FLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(FLOW, FLOW_true, decimal=4)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case4/SFLOW")
+    SFLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(SFLOW, SFLOW_true, decimal=4)
+
+    # --------------- case 5 --------------
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case5/LP")
+    LP = loadtxt(FileNameAndPath).astype(int)
+    LP -= 1  # 0-based index in python!
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case5/BLP")
+    BLP = loadtxt(FileNameAndPath)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case5/THET")
+    THET = loadtxt(FileNameAndPath)
+
+    SFLOW, FLOW = flo(LP, BLP, THET)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case5/FLOW")
+    FLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(FLOW, FLOW_true, decimal=4)
+
+    FileNameAndPath = Path(TEST_DIR, "testdata_flo/case5/SFLOW")
+    SFLOW_true = loadtxt(FileNameAndPath)
+    np.testing.assert_array_almost_equal(SFLOW, SFLOW_true, decimal=4)
