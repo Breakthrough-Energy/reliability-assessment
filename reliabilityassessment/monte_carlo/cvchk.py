b11eb4fe47ea8ae62dd7b13b5228e266e2968d59
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-12-12 16:53:05 -0600

    fix and test: avoid early termination in 'cvchk' and add more unit tests (#89)

--- reliabilityassessment/monte_carlo/cvchk.py
+++ reliabilityassessment/monte_carlo/cvchk.py
@@ -1,23 +1,22 @@
-def cvchk(CLOCK, FINISH, SUM, XLAST, SSQ, IYEAR, CVTEST):
+def cvchk(CLOCK, FINISH, IYEAR, CVTEST, SUM, XLAST, SSQ):
     """
-    Check convergence of the sequential Monte Carlo simulation. The formulation of
+    Check the convergence of the sequential Monte Carlo simulation. The formulation of
     convergence criteria refers to the slide in the following directory
     https://www.dropbox.com/s/mbooxvj85jb2oi2/module_4-3.pdf?dl=0, on page 25,
     26 and 31.
 
     :param int CLOCK: global time clock used in the simulation
     :param int FINISH: number of hours to terminate if fails to converge, typically
-        integer multiples of total hours of a year
+        integer multiples of the total hours of a year
+    :param int IYEAR: integer index of the year
+    :param float CVTEST: threshold value for convergence, defaults to 0.025 if 0 is set
     :param float SUM: cumulative quantities for a specific reliability index
     :param float XLAST: reliability index of the last simulation year
     :param float SSQ: cumulative square error of the specific reliability index
-    :param int IYEAR: integer index of the year
-    :param float CVTEST: threshold value for convergence, defaults to 0.025 if 0 is set
     :return: (*tuple*) -- a tuple of three scalars, RFLAG: an integer indicator of
         “early return or not” and updated SSQ, XLAST
 
     .. note:: Variable names may not be the same as the ones in the reference slides
-    .. todo:: IYEAR is possibly 1-based, need to check later in the integration test
     """
 
     RFLAG = int(CLOCK >= FINISH)
@@ -32,13 +31,11 @@ def cvchk(CLOCK, FINISH, SUM, XLAST, SSQ, IYEAR, CVTEST):
         return RFLAG, SSQ, XLAST
 
     XMEAN = SUM / IYEAR
-    # edge case, to avoid permanent-loop
-    if XMEAN == 0.0:
-        return RFLAG, SSQ, XLAST
+
+    XX = CVTEST * XMEAN if CVTEST else 0.025 * XMEAN
 
     STDERR = pow(((SSQ / IYEAR) - XMEAN**2) / IYEAR, 0.5)
 
-    XX = CVTEST * XMEAN if CVTEST else 0.025 * XMEAN
     # how many multiples of our "COV" to the given threshold
     XKCPXX = STDERR / XX if XX else 0.0
 
@@ -46,6 +43,15 @@ def cvchk(CLOCK, FINISH, SUM, XLAST, SSQ, IYEAR, CVTEST):
         f"{IYEAR}-th REPLICATION; SUM={SUM} NEW={DIFF} MEAN={XMEAN} CONVERGENCE="
         f"{XKCPXX}"
     )
+
+    # edge case, to avoid permanent-loop
+    if XMEAN == 0.0:
+        return RFLAG, SSQ, XLAST
+
+    # The following part exists in the original Fortran code, but since 'STDERR'
+    # will not be negative based on theoretical proof, so we comment here
+    # if STDERR < 0.0:
+    #     return RFLAG, SSQ, XLAST
 
     if STDERR < XX:
         RFLAG = 1
@@ -55,26 +61,25 @@ def cvchk(CLOCK, FINISH, SUM, XLAST, SSQ, IYEAR, CVTEST):
     return RFLAG, SSQ, XLAST
 
 
-def _cvchk(CLOCK, FINISH, SUM, XLAST, SSQ, IYEAR, CVTEST):
+def _cvchk(CLOCK, FINISH, IYEAR, CVTEST, SUM, XLAST, SSQ):
     """
-    Check convergence of the sequential Monte Carlo simulation. The formulation of
+    Check the convergence of the sequential Monte Carlo simulation. The formulation of
     convergence criteria refers to the slide in the following directory
     https://www.dropbox.com/s/mbooxvj85jb2oi2/module_4-3.pdf?dl=0, on page 25,
     26 and 31.
 
     :param int CLOCK: global time clock used in the simulation
     :param int FINISH: number of hours to terminate if fails to converge, typically
-        integer multiples of total hours of a year
+        integer multiples of the total hours of a year
+    :param int IYEAR: integer index of the year
+    :param float CVTEST: threshold value for convergence, defaults to 0.025 if 0 is set
     :param float SUM: cumulative quantities for a specific reliability index
     :param float XLAST: reliability index of the last simulation year
     :param float SSQ: cumulative square error of the specific reliability index
-    :param int IYEAR: integer index of the year
-    :param float CVTEST: threshold value for convergence, defaults to 0.025 if 0 is set
     :return: (*tuple*) -- a tuple of three scalars, RFLAG: an integer indicator of
         “early return or not” and updated SSQ, XLAST
 
     .. note:: Variable names may not be the same as the ones in the reference slides
-    .. todo:: IYEAR is possibly 1-based, need to check later in the integration test
     """
 
     RFLAG = 0
@@ -87,7 +92,7 @@ def _cvchk(CLOCK, FINISH, SUM, XLAST, SSQ, IYEAR, CVTEST):
     SSQ = SSQ + DIFF**2
 
     if (
-        IYEAR < 5  # we might want to revisit if IYEAR is 0 based.
+        IYEAR < 5
     ):  # window size for statistics cannot be less than 5 years (simulation runs)
         return RFLAG, SSQ, XLAST
 
@@ -119,6 +124,12 @@ def _cvchk(CLOCK, FINISH, SUM, XLAST, SSQ, IYEAR, CVTEST):
 
     if XMEAN == 0.0:  # edge case, to avoid permanent-loop
         return RFLAG, SSQ, XLAST
+
+    # The following part exists in the original Fortran code, but since 'STDERR'
+    # will not be negative based on theoretical proof, so we comment here
+    # if STDERR < 0.0:
+    #     return RFLAG, SSQ, XLAST
+
     if STDERR < XX:
         RFLAG = 1
         print("**************** SIMULATION CONVERGED ****************")
