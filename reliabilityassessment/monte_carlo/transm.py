4bf006d1a172946fe706552499f2cd6c42fd30e5
yonglizhu <45750983+yonglizhu@users.noreply.github.com>
2022-12-17 21:35:13 -0600

    perf: vectorization

--- reliabilityassessment/monte_carlo/transm.py
+++ reliabilityassessment/monte_carlo/transm.py
@@ -42,25 +42,19 @@ def transm(
     LCLOCK = CLOCK
     JHOUR = JHOUR
     IOI = IOI
-    NOAREA = BN.shape[0]
-    NN = NOAREA
-    NLINES = LNSTAT.shape[0]
-    NL = NLINES
+    NN = NOAREA = BN.shape[0]
+    NL = NLINES = LNSTAT.shape[0]
     BLP = np.zeros((NLINES, 3))  # in original Fortran, np.zeros((100,3))
 
     if IFLAG != 1:
-        for i in range(NL):
-            BLP[i, 0] = BLPA[i, 0]
-            BLP[i, 1] = BLPA[i, 1]
-            BLP[i, 2] = BLPA[i, 2]
+        BLP[:NL, :3] = BLPA[:NL, :3].copy()
         # CALL TM1(BN,LP,BLP,BLP0,NN,NL,BB,ZB,LT,NR)
         BLP0[:], BB[:, :], LT[:], ZB[:, :] = tm1(BN, LP, BLP, NR)
 
-    for i in range(NL):
-        L = (LNSTAT[i] - 1) * 3  # LNSTAT[i] is 1-based value
-        BLP[i, 0] = BLPA[i, L + 0]
-        BLP[i, 1] = BLPA[i, L + 1] * STMULT[i, 0]
-        BLP[i, 2] = BLPA[i, L + 2] * STMULT[i, 1]
+    L = (LNSTAT[:NL] - 1) * 3  # LNSTAT[i] is 1-based value
+    BLP[:NL, 0] = BLPA[range(NL), L]
+    BLP[:NL, 1] = BLPA[range(NL), L + 1] * STMULT[:NL, 0]
+    BLP[:NL, 2] = BLPA[range(NL), L + 2] * STMULT[:NL, 1]
 
     # local variable
     BNS = np.zeros((NOAREA, 6))  # in original Fortran, np.zeros((20,6))
@@ -76,13 +70,8 @@ def transm(
             BNS[i, 4] = SYSCON[i]
             BNS[i, 5] = CAPREQ[i]
             X = SYSCON[i] - CAPREQ[i]
-
-            if X >= 0:
-                if BN[i, 2] < 0:
-                    BN[i, 2] = 0.0
-            else:
-                if X > BN[i, 2]:
-                    BN[i, 2] = X
+            X = min(X, 0)
+            BN[i, 2] = max(BN[i, 2], X)
 
         FLOW, INDIC = tm2(
             BN,
@@ -106,41 +95,28 @@ def transm(
         NLS = NLST
         KFLAG = 1
 
-        SADJ = np.zeros(NN)
-        for i in range(NN):
-            SADJ[i] = SYSCON[i] - CAPREQ[i] - BN[i, 1]
-            if SADJ[i] < 0:
-                KFLAG = 0
+        SADJ = SYSCON[:NN] - CAPREQ[:NN] - BN[:NN, 1]
+        KFLAG = int((SADJ[:NN] >= 0).all())
 
         if KFLAG == 1:
             return JFLAG, FLOW, SADJ
 
         JFLAG = 1
         CADJ = np.zeros(NN)  # CADJ is locally used
-        for i in range(NN):
-            CADJ[i] = BN[i, 1] * NLS
+        CADJ[:NN] = BN[:NN, 1] * NLS
 
-    for i in range(NN):
-        BN[i, 2] = SYSCON[i] - CAPREQ[i] - CADJ[i]
-        BN[i, 1] = CAPREQ[i]
-        BNS[i, 0] = BN[i, 2]
-        BNS[i, 1] = BN[i, 1]
-        BNS[i, 3] = BN[i, 3]
+    BN[:NN, 2] = SYSCON[:NN] - CAPREQ[:NN] - CADJ[:NN]
+    BN[:NN, 1] = CAPREQ[:NN].copy()
+    BNS[:NN, [0, 1, 3]] = BN[:NN, [2, 1, 3]]
 
     if NLS != 0:
-        for i in range(NL):
-            j = LP[i, 1]
-            K = LP[i, 2]
-            BLP[i, 1] -= FLOW[i]
-            BLP[i, 2] += FLOW[i]
-            if BLP[i, 1] < 0.0:
-                BLP[i, 1] = 0.0
-            if BLP[i, 2] < 0.0:
-                BLP[i, 2] = 0.0
-            BN[j, 3] += FLOW[i]
-            BN[j, 4] -= FLOW[i]
-            BN[K, 3] -= FLOW[i]
-            BN[K, 4] += FLOW[i]
+        BLP[:NL, 1] -= FLOW[:NL]
+        BLP[:NL, 2] += FLOW[:NL]
+        BLP[:NL, [1, 2]] = BLP[:NL, [1, 2]].clip(min=0.0)
+        BN[LP[:NL, 1], 3] += FLOW[:NL]
+        BN[LP[:NL, 1], 4] -= FLOW[:NL]
+        BN[LP[:NL, 2], 3] += FLOW[:NL]
+        BN[LP[:NL, 2], 4] -= FLOW[:NL]
 
     FLOW, INDIC = tm2(
         BN,
@@ -161,11 +137,9 @@ def transm(
         PCTAVL,
     )
 
-    for i in range(NN):
-        BN[i, 3] = BNS[i, 3]
-        BN[i, 4] = BNS[i, 3]  # a typo ?
+    BN[:NN, 3] = BNS[:NN, 3].copy()
+    BN[:NN, 4] = BNS[:NN, 3].copy()
 
-    for i in range(NN):
-        SADJ[i] = BN[i, 1] - SYSCON[i] + CAPREQ[i] + CADJ[i]
+    SADJ[:NN] = BN[:NN, 1] - SYSCON[:NN] + CAPREQ[:NN] + CADJ[:NN]
 
     return JFLAG, FLOW, SADJ
